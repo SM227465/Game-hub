a5b639e1bf68bb71da734dfdcdbb5ccaec43da36
Sourav Mandal <sm227465@gmail.com>
2023-03-18 22:08:29 +0530

    Theme switch btn add

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
 import { Image } from '@chakra-ui/image';
-import { HStack, Text } from '@chakra-ui/layout';
+import { HStack } from '@chakra-ui/layout';
 import logo from '../assets/logo.webp';
+import ColorModeSwitch from './ColorModeSwitch';
 function NavBar() {
   return (
-    <HStack>
+    <HStack justifyContent='space-between' padding='10px'>
       <Image src={logo} boxSize='60px' />
-      <Text>NavBar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 }
