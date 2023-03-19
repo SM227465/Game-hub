d28eb5081398f0920505a254b88f6258ae9eac4a
Sourav Mandal <sm227465@gmail.com>
2023-03-19 18:59:14 +0530

    Display critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,8 +1,9 @@
 import { Card, CardBody } from '@chakra-ui/card';
 import { Image } from '@chakra-ui/image';
-import { Heading, Text } from '@chakra-ui/layout';
+import { Heading, HStack, Text } from '@chakra-ui/layout';
 import React from 'react';
 import { Game } from '../hooks/useGames';
+import CriticScore from './CriticScore';
 import PlatformIconList from './PlatformIconList';
 
 interface Props {
@@ -17,7 +18,10 @@ const GameCard = (props: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
-        <PlatformIconList platforms={game.parent_platforms.map((pp) => pp.platform)} />
+        <HStack justifyContent='space-between'>
+          <PlatformIconList platforms={game.parent_platforms.map((pp) => pp.platform)} />
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
