c33fe91fa3fc7798abed7605c4d085caebe96b8e
Sourav Mandal <sm227465@gmail.com>
2023-03-25 20:31:45 +0530

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,8 +1,14 @@
-const getCroppedImageUrl = (url: string) => {
-  const target = 'media/';
-  const index = url?.indexOf(target) + target.length;
+import noImage from '../assets/no-image-placeholder.webp';
 
-  return url?.slice(0, index) + 'crop/600/400/' + url?.slice(index);
+const getCroppedImageUrl = (url: string) => {
+  if (!url) {
+    return noImage;
+  }
+
+  const target = 'media/';
+  const index = url.indexOf(target) + target.length;
+
+  return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
 };
 
 export default getCroppedImageUrl;
