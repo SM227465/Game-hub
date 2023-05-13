bddbedb2c0362e10c292faae7c89cfc94f870974
Sourav Mandal <sm227465@gmail.com>
2023-05-13 19:42:36 +0530

    Refactor: remove duplicate interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import NavBar from './components/NavBar';
 import Pagination from './components/Pagination';
 import PlatformSelector from './components/PlatformSelector';
 import SortSelector from './components/SortSelector';
-import { Platform } from './hooks/useGames';
+import { Platform } from './hooks/usePlatforms';
 import { Genre } from './hooks/useGenres';
 
 export interface GameQuery {
